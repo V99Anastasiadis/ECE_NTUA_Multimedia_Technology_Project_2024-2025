371474206434bd33c750c521f6e99fd1a801d582
V99Anastasiadis <anastasvasilis4@gmail.com>
2024-11-07 22:02:16 +0200

    attemp creating list of priorities with the defaul already inside

--- Priority.java
+++ Priority.java
@@ -1,11 +1,21 @@
 public class Priority {
-    private String name;
+    String PriorityName;
+    List<Priority> priorities;
 
-    public String getName() {
-        return name;
+
+    public Priority() {
+        priorities = new ArrayList<>(); // Αρχικοποίηση της λίστας
+        priorities.add(this); // Προσθήκη του αντικειμένου ως default
+    }
+
+    public List<Priority> getPriorities() { // Getter για τη λίστα priorities
+        return priorities;
     }
 
     public void setName(String name) {
         this.name = name;
     }
+
+    priorities.add(new Priorities());
+//check chatgtp for information about priorities defaul
 }
