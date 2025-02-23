d54761c56e3d679475ae8ec7c104f3540b661d0d
el19954 <el19954@mail.ntua.gr>
2025-02-23 18:38:09 +0200

    remove App class and add Controller class to manage UI interactions

---
+++ task-manager/src/main/java/com/taskmanager/Controller.java
@@ -0,0 +1,9 @@
+//package task-manager.src.main.java.com;
+
+public class Controller {
+    /*
+     * Το Controller.java είναι η "γέφυρα" μεταξύ του Frontend (FXML) και του Backend (Java Code).
+     * Ουσιαστικά, όταν ο χρήστης αλληλεπιδρά με το UI (π.χ., πατάει κουμπιά, βλέπει δεδομένα σε πίνακα), 
+     * ο Controller διαχειρίζεται αυτά τα γεγονότα και εκτελεί ενέργειες στην εφαρμογή.
+     */
+}
